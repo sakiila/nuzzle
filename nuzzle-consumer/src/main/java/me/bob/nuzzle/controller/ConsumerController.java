746d8a5e254abcec6aa7e21b283c7bbe9b71802b
sakiila <imbaobo@gmail.com>
2021-11-06 23:05:56 +0800

    first edition: use socket

--- nuzzle-consumer/src/main/java/me/bob/nuzzle/controller/ConsumerController.java
+++ nuzzle-consumer/src/main/java/me/bob/nuzzle/controller/ConsumerController.java
@@ -1,16 +1,35 @@
 package me.bob.nuzzle.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import me.bob.nuzzle.api.UserService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 @RestController
+@Slf4j
 public class ConsumerController {
 
     private UserService userService;
 
     @RequestMapping("/hello")
     public String sayHi(String name) {
+        try (Socket socket = new Socket("127.0.0.1", 8090)) {
+
+            final ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+            final String request = "Hello!";
+            objectOutputStream.writeObject(request);
+
+            final ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+            final String messaage = (String) objectInputStream.readObject();
+            return messaage;
+        } catch (IOException | ClassNotFoundException e) {
+            log.error("Exception", e);
+        }
         return "";
     }
 
---
+++ nuzzle-provider/src/main/java/me/bob/nuzzle/server/ProviderServer.java
@@ -0,0 +1,39 @@
+package me.bob.nuzzle.server;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+@Service
+@Slf4j
+public class ProviderServer {
+
+    @PostConstruct
+    public void init() {
+        try (ServerSocket server = new ServerSocket(8099)) {
+            Socket socket;
+            while ((socket = server.accept()) != null) {
+                log.info("client connected");
+                try (final ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream())) {
+                    final String messaage = (String) objectInputStream.readObject();
+                    log.info("message is: {}", messaage);
+                    
+                    final ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+                    final String response = "Hola!";
+                    objectOutputStream.writeObject(response);
+                    objectOutputStream.flush();
+                } catch (IOException | ClassNotFoundException e) {
+                    log.error("Exception", e);
+                }
+            }
+        } catch (IOException e) {
+            log.error("Exception", e);
+        }
+    }
+}
