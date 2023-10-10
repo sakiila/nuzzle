75a4cdc9fc8fd276beccfdc86f24a03cd06c6fab
sakiila <imbaobo@gmail.com>
2023-10-10 15:33:01 +0800

    update

--- nuzzle-core/src/main/java/me/bob/nuzzle/registry/NuzzleServiceDiscovery.java
+++ nuzzle-core/src/main/java/me/bob/nuzzle/registry/NuzzleServiceDiscovery.java
@@ -7,7 +7,7 @@ public class NuzzleServiceDiscovery {
 
     public static InetSocketAddress lookupService(String serviceName) {
         final List<String> node = CuratorUtils.getNode(serviceName);
-        // TODO: 2021/11/8 loadbalance 
+        // TODO: 2021/11/8 load-balance
         String address = node.get(0);
         final String[] split = address.split(":");
         return new InetSocketAddress(split[0], Integer.parseInt(split[1]));
